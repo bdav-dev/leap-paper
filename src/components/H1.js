2df7147be2b375ead9385fbfd2ab9590e6f615a3
David Berezowski <david.berezowski1@gmail.com>
2024-06-04 13:49:02 +0200

    Removed compiled .js files which caused issues

--- src/components/H1.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var jsx_runtime_1 = require("react/jsx-runtime");
-function H1(props) {
-    return ((0, jsx_runtime_1.jsxs)("div", { className: "mb-2", children: [(0, jsx_runtime_1.jsx)("h1", { className: "text-2xl mb-[0.05rem]", children: props.children }), (0, jsx_runtime_1.jsx)("hr", { className: "border-none bg-zinc-800 h-[0.1rem] rounded-full" })] }));
-}
-exports.default = H1;
--- src/components/Panel.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var jsx_runtime_1 = require("react/jsx-runtime");
-function Panel(props) {
-    return ((0, jsx_runtime_1.jsx)("div", { className: "bg-zinc-100 p-3 rounded-lg " + props.className, children: props.children }));
-}
-exports.default = Panel;
